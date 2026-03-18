b1b78b160adda7686856e9bd805399fbec234775
luoaowoo <luoaowoo@outlook.com>
2026-03-18 17:36:26 +0800

    Update themeConfig.mjs

--- .vitepress/theme/assets/themeConfig.mjs
+++ .vitepress/theme/assets/themeConfig.mjs
@@ -360,10 +360,14 @@ export const themeConfig = {
       name: "洛嗷呜", //要显示的名字
       event: "活着", //事件名称
       date: "2010-03-17", //出生日期
+      
+    },
+    timing: {
       enable: true,
       name: "洛嗷呜", //要显示的名字
       event: "活着", //事件名称
       date: "2010-03-17", //出生日期
+      
     },
     // 站点数据
     siteData: {
